4545cd86c1fe2a495a66553072a307cbe41929e2
kongtianyi14 <134493346+kongtianyi14@users.noreply.github.com>
2023-09-21 17:05:59 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,18 @@
+"""
+    函数
+        input
+"""
+# 1.字面意思
+# 2.作用
+# print()#换行
+# 3.语法
+# 4.适用性
+# ex2(1)
+# name = input("请输入姓名:")
+# age = input("请输入年龄:")
+# print(name + "的年龄是:" + age + ".")
+# ex2(2)
+A = input("请输入主语：")
+B = input("请输入谓语：")
+C = input("请输入宾语：")
+print(A + B + C)
